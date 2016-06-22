773731fecf4a472f5c3fe9f92f4626dea38bfc8f
Nelson R Monserrate <monserratenelson@johndeere.com>
2016-06-22 17:38:31 -0500

    Allocate initial_setup() inside setup.py Re-enabled the read() function inside setup.py
    Using utils.initial_setup() was causing dependency issues when installing the
    package. Mainly because setup.py needs to install the packages on the
    requirement list before loading the utils module.

--- pulpadm/utils.py
+++ pulpadm/utils.py
@@ -3,8 +3,6 @@ from builtins import open
 import os
 import logging
 import yaml
-from shutil import copyfile
-from pulpadm.constants import BASE_DIR, TMPL_DIR
 
 
 def read_yaml(path=None):
@@ -49,22 +47,3 @@ def read_file(path=None):
         logger.error(e)
     finally:
         return data
-
-
-def initial_setup():
-    """
-    Setup BASE_DIR and example files
-    """
-    print()
-    if not os.path.exists(BASE_DIR):
-        print("Creating {0}".format(BASE_DIR))
-        os.mkdir(BASE_DIR)
-
-    for fname in ["config.yaml", "repos.yaml"]:
-        src = os.path.join(TMPL_DIR, fname)
-        dst = os.path.join(BASE_DIR, fname)
-        if os.path.exists(dst):
-            dst += ".new"
-        print("Copying {0} -> {1}".format(os.path.basename(src), dst))
-        copyfile(src, dst)
-    print()
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import os
 import sys
+from shutil import copyfile
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
-from pulpadm.utils import initial_setup, read_file as read
-from pulpadm.constants import PKG_NAME, PKG_DESC
+from pulpadm.constants import PKG_NAME, PKG_DESC, BASE_DIR, TMPL_DIR
 
 
 # Global variables
 #
-version = "0.1.0"
+version = "0.1.1"
 requires = [
     "future",
     "requests",
@@ -19,6 +19,39 @@ requires = [
 # Python v2.6 does not include argparse on the stdlib
 if sys.version_info[:2] == (2, 6):
     requires.append("argparse>=1.1")
+
+
+def read(fname):
+    """
+    Utility function to read the README file (long_description)
+    It's nice, because now:
+      1) we have a top level README
+      2) it's easier to type in the README than to put a raw string in below
+    """
+    data = ""
+    with open(os.path.join(os.path.dirname(__file__), fname), 'r') as stream:
+        data = stream.read()
+    stream.closed
+    return data
+
+
+def initial_setup():
+    """
+    Setup BASE_DIR and example files
+    """
+    print()
+    if not os.path.exists(BASE_DIR):
+        print("Creating {0}".format(BASE_DIR))
+        os.mkdir(BASE_DIR)
+
+    for fname in ["config.yaml", "repos.yaml"]:
+        src = os.path.join(TMPL_DIR, fname)
+        dst = os.path.join(BASE_DIR, fname)
+        if os.path.exists(dst):
+            dst += ".new"
+        print("Copying {0} -> {1}".format(os.path.basename(src), dst))
+        copyfile(src, dst)
+    print()
 
 
 class PostDevelopCommand(develop):
@@ -42,7 +75,7 @@ class PostInstallCommand(install):
 setup(
     name=PKG_NAME,
     description=PKG_DESC,
-    long_description=read(os.path.join(os.path.dirname(__file__), "README")),
+    long_description=read("README"),
     version=version,
     author="Nelson R Monserrate",
     author_email="nrmonserrate@gmail.com",
